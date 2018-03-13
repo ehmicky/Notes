1054cf038c7f3969e3c3e2116c14b740689c96a1
ehmicky <ehmicky@gmail.com>
2018-03-13 17:00:00 +0100

    Update README index

--- Repository_helpers/Index/create_index.sh
+++ Repository_helpers/Index/create_index.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Creates a list of files at index.md
 
+echo "yo"
 dir="Repository_helpers/Index"
 if [[ ! -f "$dir/index.md" ]]; then
   echo "Error: current directory must be the project root" >&2
   exit 1
 fi
-echo "yo"
 
 md-file-tree | sed -f "$dir/create_index.sed" > "$dir/index.md"
 
